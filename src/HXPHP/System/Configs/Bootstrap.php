8041a3ec5d4bbf4c6ed756ead3c357d0363eb6ec
Bruno Santos <bruno@plug.ag>
2018-02-26 00:38:12 -0300

    Changed HXPHP Version

--- src/HXPHP/System/Configs/Bootstrap.php
+++ src/HXPHP/System/Configs/Bootstrap.php
@@ -18,7 +18,7 @@ class Bootstrap
             putenv('ROOT_PATH=' . ROOT_PATH . static::DS);
             putenv('APP_PATH=' . getenv('ROOT_PATH') . 'app' . static::DS);
             putenv('TEMPLATES_PATH=' . getenv('ROOT_PATH') . 'templates' . static::DS);
-            putenv('HXPHP_VERSION=3.0.0-rc.6');
+            putenv('HXPHP_VERSION=3.0.0-rc.7');
         }   
     }
 }
