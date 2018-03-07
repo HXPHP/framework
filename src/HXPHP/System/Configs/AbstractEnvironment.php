5504561f2a2a7012c41f397dbe71fcc409663684
Jorge Carlos <skay_1994@yahoo.com.br>
2018-03-06 23:51:47 -0300

    Carregamento e registro de Modulos por composer e local

--- src/HXPHP/System/Configs/AbstractEnvironment.php
+++ src/HXPHP/System/Configs/AbstractEnvironment.php
@@ -1,6 +1,8 @@
 <?php
 namespace HXPHP\System\Configs;
 
+use HXPHP\System\Tools;
+
 abstract class AbstractEnvironment
 {
     public $baseURI;
@@ -12,5 +14,55 @@ abstract class AbstractEnvironment
 
         $load = new LoadModules;
         return $load->loadModules($this);
+    }
+
+    public function registerModule(string $type, string $module, string $name = null)
+    {
+        switch ($type){
+            case 'local':
+                $module_class = Tools::filteredName(ucwords($module));
+
+                if($name){
+                    $module = $name;
+                }
+
+                if (!class_exists($module_class))
+                    throw new \Exception("O modulo local <'$module_class'> informado nao existe.", 1);
+                else
+                    $this->$module = new $module_class();
+
+                break;
+            case 'composer':
+                $module_class = Tools::filteredName(ucwords($module));
+                $object = $module_class.'\src\Config';
+
+                if($name){
+                    $module = $name;
+                }
+
+                if (!class_exists($object))
+                    throw new \Exception("O modulo composer <'$object'> informado nao existe.", 1);
+                else
+                    $this->$module = new $object();
+                break;
+        }
+
+        return $this;
+    }
+
+    public function registerModules(array $modules)
+    {
+        foreach ($modules as $module => $info){
+            $type = $info['type'] ?? '';
+            $name = $info['name'] ?? '';
+
+            if(empty($type)){
+                continue;
+            }
+
+            $data = [$type,$module,$name];
+
+            call_user_func_array([$this,'registerModule'],$data);
+        }
     }
 }
--- src/HXPHP/System/Controller.php
+++ src/HXPHP/System/Controller.php
@@ -80,34 +80,114 @@ class Controller
          * @var array
          */
         $args = func_get_args();
-        $object = $args[0];
+        $type = $args[0];
 
-        /**
-         * Define os demais argumentos passados como
-         * parâmetros para o construtor do objeto injetado
-         */
-        unset($args[0]);
-        $params = !($args) ? [] : array_values($args);
+        if(!in_array($type, ['hxphp', 'composer', 'local'])){
+            $type = 'hxphp';
+
+            $object = $args[0];
+
+            unset($args[0]);
+        } else {
+            $object = $args[1];
+
+            unset($args[0]);
+            unset($args[1]);
+        }
+
+        $args = array_values($args);
+
+        $moduleName = null;
+
+        if(isset($args[0]['name'])){
+            $moduleName = $args[0]['name'];
+            unset($args[0]);
+
+            $args = array_values($args);
+        }
 
         /**
          * Tratamento que adiciona a pasta do módulo
          */
         $explode = explode('\\', $object);
-        $object = $object . '\\' . end($explode);
-        $object = 'HXPHP\System\\' . $object;
 
-        if (class_exists($object)) {
+        if($type === 'hxphp')
+        {
+            $object = 'HXPHP\System\\' . $object;
+            $object = $object . '\\' . end($explode);
+
+            if(!$moduleName)
+                $moduleName = end($explode);
+        }
+
+        if($type === 'composer')
+        {
+            if(count($explode) == 1)
+            {
+                $module = $explode[0];
+                $object = $module.'\src\\'.$module;
+
+                if(!$moduleName)
+                    $moduleName = $module;
+
+            } else {
+                $object = implode('\\',$explode);
+
+                if(!$moduleName)
+                    $moduleName = $explode[0];
+            }
+        }
+
+        if($type === 'local')
+        {
+            if(count($explode) == 1)
+            {
+                $module = $explode[0];
+                $object = $module.'\src\\'.$module;
+
+                if(!$moduleName)
+                    $moduleName = $module;
+
+            } else {
+                $object = implode('\\',$explode);
+
+                if(!$moduleName)
+                    $moduleName = $explode[0];
+            }
+        }
+
+        /**
+         * Define os demais argumentos passados como
+         * parâmetros para o construtor do objeto injetado
+         */
+        $params = !($args) ? [] : array_values($args);
+
+        if (class_exists($object))
+        {
             $name = end($explode);
             $name = strtolower(Tools::filteredName($name));
 
-            if ($params) {
+            $enviroment = $this->configs->define->getDefault();
+            
+            if ($params)
+            {
+                var_dump($params);
                 $ref = new \ReflectionClass($object);
                 $this->view->$name = $ref->newInstanceArgs($params);
-            } else
-                $this->view->$name = new $object();
+            } else {
+
+                if(isset($this->configs->env->$enviroment->$moduleName))
+                {
+                    $this->view->$name = new $object($this->configs->env->$enviroment->$moduleName);
+                } else {
+                    $this->view->$name = new $object();
+                }
+            }
 
             return $this->view->$name;
         }
+
+        return null;
     }
 
     /**
