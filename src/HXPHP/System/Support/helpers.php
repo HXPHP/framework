6903449af6eff808f4c6c41dcdca971574993f5c
Bruno Santos <bruno@plug.ag>
2018-05-04 16:46:37 -0300

    Added dd() helper

--- src/HXPHP/System/Support/helpers.php
+++ src/HXPHP/System/Support/helpers.php
@@ -1,5 +1,24 @@
 <?php
 
+use HXPHP\System\Support\Debug\Dumper;
+
+if (! function_exists('dd')) {
+    /**
+     * Dump the passed variables and end the script.
+     *
+     * @param  mixed
+     * @return void
+     */
+    function dd()
+    {
+        array_map(function ($x) {
+            (new Dumper)->dump($x);
+        }, func_get_args());
+
+        die(1);
+    }
+}
+
 if (!function_exists('getEnvValue')) {
 	/**
 	 * Check an env. var is set, else return default value
